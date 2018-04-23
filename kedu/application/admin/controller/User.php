14d4f34096041958599a2bf9046eba2508940a77
houjunwei <541567143@qq.com>
2018-04-23 15:54:48 +0800

    侯钧威修改后提交

--- kedu/application/admin/controller/User.php
+++ kedu/application/admin/controller/User.php
@@ -132,7 +132,7 @@ class User extends Base
             //查出用户表中所有等级
             $grade=Db::name('user_grade')->select();
 
-            return $this->fetch('save', ['message' => Session::get('message'),'city'=>$city,'area'=>$area,'province'=>$province,'grade'=>$grade]);
+            return $this->fetch('save', ['message' => Session::get('message'),'city'=>$city,'area'=>$area,'province'=>$province,'grade'=>$grade,'data'=>$data]);
 
         } elseif ($request->isPost()) {
 
